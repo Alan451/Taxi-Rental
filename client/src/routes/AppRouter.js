52a56ebf44a3c7e33e6bae40c5b5963585cc2dfb
adriraj2000 <adrirajc@gmail.com>
2021-02-05 21:49:33 +0530

    Student and Driver homepage

--- client/src/routes/AppRouter.js
+++ client/src/routes/AppRouter.js
@@ -4,14 +4,16 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 //Pages
 import ForgotPassword from '../pages/ForgotPassword'
 import Login from '../pages/Login'
-
-
+import Student from '../pages/StudentPage'
+import Driver from '../pages/DriverPage'
 const AppRouter = () => (
     <BrowserRouter>
       <div>
         <Switch>
           <Route path="/" component={Login} exact />
           <Route path="/forgotpassword" component={ForgotPassword} />
+          <Route path="/student" component = {Student} />
+          <Route path="/driver" component = {Driver} />
         </Switch>
       </div>
     </BrowserRouter>
